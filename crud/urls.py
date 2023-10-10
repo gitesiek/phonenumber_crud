76d8bd2bcd8933eafa01a2aaac0fd8f403f8947f
Karol Gluch <gitesiek@gmail.com>
2023-10-10 08:11:19 +0200

    Implemented native django login system

--- crud/urls.py
+++ crud/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-from .views import clinic_list
+from django.urls import path, include
+from .views import clinic_list, login_page
 
 urlpatterns = [
     path('', clinic_list, name='clinic_list'),
+    path('login', login_page, name='clinic_list'),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Clinic
 
 def clinic_list(request):
     clinics = Clinic.objects.all()
     return render(request, 'home.html', {'clinics': clinics})
+
+def login_page(request):
+    return redirect('accounts/login')
