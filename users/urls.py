e491f91386a062692cf416a389f32bad38e12a39
Karol Gluch <gitesiek@gmail.com>
2023-12-11 14:27:06 +0100

    Added urls for new pages

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,24 @@
 from django.urls import path, include   
-from .views import login_page
+from .views import (
+    login_page,
+    user_create, user_delete, user_details, user_edit, user_list,
+    group_create, group_delete, group_details, group_edit, group_list,
+)
 
 
 urlpatterns = [
     path('login', login_page, name='login_page'),
     path("accounts/", include("django.contrib.auth.urls")),
+
+    path('user_list', user_list, name='user_list_page'),
+    path('user/create', user_create),
+    path('user/delete', user_delete),
+    path('user/details', user_details),
+    path('user/edit', user_edit),
+
+    path('group_list', group_list, name='group_list_page'),
+    path('group/create', group_create),
+    path('group/delete', group_delete),
+    path('group/details', group_details),
+    path('group/edit', group_edit),
 ]
--- users/views.py
+++ users/views.py
@@ -3,3 +3,25 @@ from django.shortcuts import render, redirect
 
 def login_page(request):
     return redirect('accounts/login')
+
+def user_create(request):
+    return render(request, 'usersViews/create.html')
+def user_delete(request):
+    return render(request, 'usersViews/delete.html')
+def user_details(request):
+    return render(request, 'usersViews/details.html')
+def user_edit(request):
+    return render(request, 'usersViews/edit.html')
+def user_list(request):
+    return render(request, 'usersViews/user_list.html')
+
+def group_create(request):
+    return render(request, 'rolesViews/create.html')
+def group_delete(request):
+    return render(request, 'rolesViews/delete.html')
+def group_details(request):
+    return render(request, 'rolesViews/details.html')
+def group_edit(request):
+    return render(request, 'rolesViews/edit.html')
+def group_list(request):
+    return render(request, 'rolesViews/group_list.html')
